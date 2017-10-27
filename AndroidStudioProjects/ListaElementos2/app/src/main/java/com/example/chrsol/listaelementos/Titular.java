135c99d6ff265078b9899598f130b527b4e63a56
christianfus <christianfus@gmail.com>
2017-10-27 13:34:52 +0200

    Programa pasar imagen de pantalla a otra

---
+++ AndroidStudioProjects/ListaElementos2/app/src/main/java/com/example/chrsol/listaelementos/Titular.java
@@ -0,0 +1,27 @@
+package com.example.chrsol.listaelementos;
+
+import android.os.Bundle;
+
+import java.io.Serializable;
+
+public class Titular implements Serializable{
+    private String titulo;
+    private String subtitulo;
+    private int imagen;
+    public Titular(String tit, String sub, int imagen) {
+        titulo = tit;
+        subtitulo = sub;
+        this.imagen = imagen;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getSubtitulo(){
+        return subtitulo;
+    }
+    public int getImagen(){
+        return imagen;
+    }
+}
+
+
