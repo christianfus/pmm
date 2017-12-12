d487a221ee30e2415b4cb04c03203f6724869e8e
christianfus <christianfus@gmail.com>
2017-12-12 12:18:52 +0100

    Añadido proyecto SoloBiciDidadctico

---
+++ AndroidStudioProjects/SoloBiciDidactico/app/src/main/java/com/example/chrsol/solobicididactico/SoloBici.java
@@ -0,0 +1,13 @@
+package com.example.chrsol.solobicididactico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoloBici extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solo_bici);
+    }
+}
--- eclipse-workspace/GMySql/src/ArticuloUi.java
+++
@@ -1,9 +0,0 @@
-
-public class ArticuloUi {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
